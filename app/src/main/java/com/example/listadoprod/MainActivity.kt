91bcb4e718992b8fb42a3427818cc8ae7492eb5d
DylanEmil <95322178+DylanEmil@users.noreply.github.com>
2022-09-28 22:04:42 -0600

    Hola aqui presento mi Prueba de Moviles 1

---
+++ app/src/main/java/com/example/listadoprod/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.listadoprod
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.listadoprod.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import dataadapter.ProductoAdapter
+import dataclass.Producto
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var listaProd = ArrayList<Producto>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        iniciar()
+    }
+    private fun limpiar(){
+        with(binding){
+            etID.setText("")
+            etNombreProd.setText("")
+            etPrecio.setText("")
+            etID.requestFocus()
+        }
+    }
+    private fun agregaProd(){
+        with(binding){
+            try {
+                val id: Int = etID.text.toString().toInt()
+                val nombre: String = etNombreProd.text.toString()
+                val precio: Double = etPrecio.text.toString().toDouble()
+                val prod = Producto(id, nombre, precio)
+                listaProd.add(prod)
+
+            }catch (ex: Exception){
+                Toast.makeText(this@MainActivity, "Erro: ${ex.toString()} ",
+                Toast.LENGTH_LONG).show()
+            }
+
+            rcvLista.layoutManager = LinearLayoutManager(this@MainActivity)
+            rcvLista.adapter = ProductoAdapter(listaProd,
+            { producto -> onItemSelected(producto) },
+            {position -> onDeleteItem(position)},
+            {position -> onUpdateItem(position)})
+            limpiar()
+
+            }
+        }
+    fun onItemSelected(producto: Producto) {
+        with(binding) {
+            etID.text = producto.id.toString().toEditable()
+            etNombreProd.text = producto.nombre.toEditable()
+            etPrecio.text = producto.precio.toString().toEditable()
+        }
+    }
+
+    fun String.toEditable(): Editable = Editable.Factory.getInstance().newEditable(this)
+
+
+    fun onUpdateItem(position: Int){
+        with(binding){
+            val id: Int = etID.text.toString().toInt()
+            val nombre: String = etNombreProd.text.toString()
+            val precio: Double = etPrecio.text.toString().toDouble()
+            val prod = Producto(id, nombre, precio)
+            listaProd.set(position, prod)
+            rcvLista.adapter?.notifyItemChanged(position)
+        }
+    }
+
+    fun onDeleteItem(position: Int) {
+        val builder = AlertDialog.Builder(this@MainActivity)
+        builder.setMessage("Desea realmente eliminar el registro?")
+            .setCancelable(false)
+            .setPositiveButton("si"){dialog, id ->
+                with(binding) {
+                    listaProd.removeAt(position)
+                    rcvLista.adapter?.notifyItemRemoved(position)
+                }
+            }.setNegativeButton("No"){dialog, id ->
+                dialog.dismiss()
+            }
+        val alert = builder.create()
+        alert.show()
+    }
+
+    private fun iniciar(){
+        binding.btnAgregar.setOnClickListener{
+            agregaProd()
+        }
+        binding.btnLimpiar.setOnClickListener{
+            limpiar()
+        }
+    }
+
+}
